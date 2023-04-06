e8b36950a69dec2ba40e1cae6403287a9111ac86
Baldi00 <andrea.baldelli2000@gmail.com>
2023-04-06 12:51:24 +0200

    Added computers interaction

---
+++ Assets/Scripts/ComputerInteraction.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class ComputerInteraction : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private Transform monitorStation;
+	[SerializeField]
+	private Material monitorOnMaterial;
+
+    private Renderer[] screens;
+    private Renderer rend;
+    private Material material;
+    private Material monitorOffMaterial;
+
+    private bool pcAndMonitorOn;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        material = new Material(rend.material);
+
+        screens = monitorStation.GetComponentsInChildren<Renderer>().Where<Renderer>(x => x.name == "Screen").ToArray<Renderer>();
+
+        monitorOffMaterial = new Material(screens[0].material);
+        pcAndMonitorOn = false;
+	}
+
+	public void StartHovering()
+    {
+        rend.material.color = material.color * 2f;
+    }
+
+	public void StartInteraction()
+	{
+        pcAndMonitorOn = !pcAndMonitorOn;
+        foreach (Renderer screen in screens)
+            screen.material = pcAndMonitorOn ? monitorOnMaterial : monitorOffMaterial;
+	}
+
+	public void StopHovering()
+    {
+        rend.material.color = material.color;
+    }
+
+	public void StopInteraction()
+	{
+	}
+}
